d2a34f208bfef56011033cc7a779ecb2cad9998a
Suelen Braga <suelen.braga2006@gmail.com>
2017-04-13 20:50:24 -0300

    Fim da atividade

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,18 @@ var htmlmin = require('gulp-htmlmin');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var del = require('del');
+
 gulp.task('minificarhtml', function(){
     return gulp.src('./source/*.html')
                .pipe(htmlmin({collapseWhitespace: true}))
                .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('apaga',function(){
+    del('./dist/css/*.min.css');
 });
 
 gulp.task('compilarsass', function(){
@@ -17,9 +25,16 @@ gulp.task('compilarsass', function(){
                .pipe(gulp.dest('./dist/css/'));
 });
 
+gulp.task('minificarcss',['apaga','compilarsass'],function(){
+    return gulp.src('./dist/css/*css')
+           .pipe(cssmin())
+           .pipe(rename({suffix:'.min'}))
+           .pipe(gulp.dest('./dist/css/'));
+});
+
 gulp.task('monitorar', function(){
-    gulp.watch('./source/scss/**/*.scss', ['compilarsass']);
+    gulp.watch('./source/scss/**/*.scss', ['minificarcss']);
     gulp.watch('./source/*html', ['minificarhtml']);
 });
 
-gulp.task('default', ['minificarhtml', 'compilarsass', 'monitorar']);
+gulp.task('default', ['minificarhtml', 'minificarcss', 'monitorar']);
